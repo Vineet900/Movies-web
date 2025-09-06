d8aac2936ed8e6ab179db5852dc32990b09b03ce
Vineet900 <vroy15518@gmail.com>
2025-09-06 23:16:29 +0530

    Fix case-sensitive import

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import Store from "./store/store.jsx";
+import Store from "./store/Store.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={Store} >
